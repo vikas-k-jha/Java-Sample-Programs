baf92814b264a9ed738486216c680dd769673b9a
Vikas Jha (V) <v_jha@apple.com>
2018-02-18 20:59:25 -0800

    Lamda Expression demo

---
+++ src/com/vikas/java/java_8/LamdaExpressionDemo.java
@@ -0,0 +1,45 @@
+/**
+ * Example lamda expression 
+ * LamdaExpressionDemo.java
+ */
+package com.vikas.java.java_8;
+
+interface LamdaExpression {
+	void display();
+}
+
+/**
+ * <br> Problem Statement :
+ *  
+ * Lamda Expression demo
+ * 
+ * </br>
+ * 
+ * @author Vikas Jha
+ */
+public class LamdaExpressionDemo {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * 1 - Implement interface using anonymous class - traditional way
+		 * We can notice it will create anonymous class - LamdaExpressionDemo$1.class
+		 * 
+		 */
+		new LamdaExpression () {
+			@Override
+			public void display () {
+				System.out.println("Implement interface using anonymous class!");
+			}
+			
+		}.display();
+		
+		/*
+		 *  2 - Implement interface using anonymous class - lamda expression
+		 *  We can notice lamda expression will not create any anonymous class
+		 */
+		LamdaExpression lamda = () -> System.out.println("Implement interface using lamda expression!");
+		lamda.display();
+	}
+
+}
