2cb717cde4af8c8ea5ac4b6af98868016365448c
Vikas Jha (V) <v_jha@apple.com>
2018-02-18 18:29:55 -0800

    Iterating through a collection using 'forEach' v/s traditional approach.

---
+++ src/com/vikas/java/java_8/Iterable_forEachDefaultmethod.java
@@ -0,0 +1,50 @@
+/**
+ * Example 'forEach' method from Iterable interface
+ * Iterable_forEachDefaultmethod.java
+ */
+package com.vikas.java.java_8;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * <br> Problem Statement :
+ *  
+ * Iterating through a Collection in Java - using 'forEach' v/s traditional approach.
+ * 
+ * </br>
+ * 
+ * @author Vikas Jha
+ */
+public class Iterable_forEachDefaultmethod {
+
+	public static void main(String args[]) {
+		
+		List<String> stringList = Arrays.asList("ABC","DEF","XYZ","MNO");
+		
+		//1 - Iterate collection using 'for' loop
+		System.out.println("## 1 - Iterate using 'for' loop");
+		for (int i=0; i < stringList.size() ; i++) {			
+			System.out.println(stringList.get(i));
+		}
+		
+		//2 - Iterate collection using foreach loop
+		System.out.println("\n\n## 2 - Iterate using foreach loop");
+		for (String str:stringList) {
+			System.out.println(str);
+		}
+		
+		//3 - Iterate collection using iterator abstract method
+		System.out.println("\n\n## 3 -Iterate using 'iterator' abstract method");
+		Iterator<String> itr = stringList.iterator();
+		while(itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+			
+		//4 - Iterate collection using Java-8 - 'forEach' method
+		System.out.println("\n\n## 4 - Iterate using 'forEach' default method");
+		stringList.forEach(System.out::println);
+	}
+
+}
---
+++ src/com/vikas/java/java_8/package-info.java
@@ -0,0 +1,8 @@
+
+/**
+ * @author Vikas Jha
+ * 
+ * This package covers various sample programs using java-8 feature 
+ * 
+ */
+package com.vikas.java.java_8;
