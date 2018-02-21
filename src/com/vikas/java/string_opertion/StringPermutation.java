a56536f28d6d3a8be1a6f98514fa3cce31cb4fa9
Vikas Jha <vks.jha@gmail.com>
2018-02-20 22:19:10 -0800

    Permutation of given string

---
+++ src/com/vikas/java/string_opertion/StringPermutation.java
@@ -0,0 +1,35 @@
+/**
+ * Example lamda expression 
+ * StringPermutation.java
+ */
+package com.vikas.java.string_opertion;
+
+/**
+ * <br> Problem Statement :
+ *  
+ * Find the Permutation of given string
+ * 
+ * </br>
+ * 
+ * @author Vikas Jha
+ */
+public class StringPermutation {
+
+	public static void main(String[] args) {
+		String str="ABC";
+		permutation(str);
+	}
+
+	public static void permutation(String str) { 
+	    permutation("", str); 
+	}
+
+	private static void permutation(String prefix, String str) {
+	    int n = str.length();
+	    if (n == 0) System.out.println(prefix);
+	    else {
+	        for (int i = 0; i < n; i++) 
+	            permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1, n));
+	    }
+	}
+}
