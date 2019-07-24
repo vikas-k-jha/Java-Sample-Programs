1bc3b09efcc25e6db8b84f82f52e54e5ae69fc76
Vikas Jha (V) <v_jha@apple.com>
2019-07-23 20:04:51 -0700

    Program to reverse given String

--- src/com/sample/example/string/StringReverse.java
+++ src/com/sample/example/string/StringReverse.java
@@ -14,16 +14,15 @@ public class StringReverse {
 
 	public static void main(String[] args) {
 
-		System.out.println("Reverse of VIKAS : " + stringReverseOption1("VIKAS"));
-		System.out.println("Reverse of JHA : " + stringReverseOption2("JHA"));
-		System.out.println("Reverse of PRANK : " + stringReverseWithRecursion2("PRANK"));
-		System.out.print("Reverse of FRANK : ");
-		stringReverseWithRecursion1("", "FRANK");
-
+		// test case
+		System.out.println("Reverse of VIKAS : " + stringReverseWithStringBuffer("VIKAS"));
+		System.out.println("Reverse of ROHAN : " + stringReverseDivideAndSwap("ROHAN"));
+		System.out.println("Reverse of FRANK : " + stringReverseWithForwardRecursion("FRANK"));
+		System.out.println("Reverse of PRANK : " + stringReverseWithReverseRecursion("PRANK"));
 	}
 
 	// 1 - String reverse using stringBuffer
-	private final static String stringReverseOption1(String str) {
+	private final static String stringReverseWithStringBuffer(String str) {
 
 		char[] stringToReverse = str.toCharArray();
 		StringBuffer stringBuffer = new StringBuffer();
@@ -35,7 +34,7 @@ public class StringReverse {
 	}
 
 	// 2 - String reverse using divide and swap
-	private final static String stringReverseOption2(String str) {
+	private final static String stringReverseDivideAndSwap(String str) {
 
 		char[] stringToReverse = str.toCharArray();
 		int length = stringToReverse.length;
@@ -50,23 +49,19 @@ public class StringReverse {
 		return String.valueOf(stringToReverse);
 	}
 
-	// 3 - String reverse recursively
-	private final static void stringReverseWithRecursion1(String reverseString, String str) {
-
-		if (str.length() == 0)
-			System.out.print(reverseString);
-		else {
-			stringReverseWithRecursion1(reverseString + str.charAt(str.length() - 1),
-					str.substring(0, str.length() - 1));
-		}
-	}
-
-	// 4 - String reverse recursively
-	private final static String stringReverseWithRecursion2(String str) {
+	// 3 - String reverse with Forward Recursion
+	private final static String stringReverseWithForwardRecursion(String str) {
 
 		if ((str == null) || (str.length() <= 1))
 			return str;
-		return stringReverseWithRecursion2(str.substring(1)) + str.charAt(0);
+		return stringReverseWithForwardRecursion(str.substring(1)) + str.charAt(0);
 	}
 
+	// 4 - String reverse with Reverse Recursion
+	private final static String stringReverseWithReverseRecursion(String str) {
+
+		if ((str == null) || (str.length() <= 1))
+			return str;
+		return str.charAt(str.length() - 1) + stringReverseWithReverseRecursion(str.substring(0, str.length()-1));
+	}
 }
